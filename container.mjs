0a2e0a5da3ce088965120dbca7406b35a3c76e4c
terrierscript <13282103+terrierscript@users.noreply.github.com>
2018-06-22 10:01:32 +0900

    wip

--- container.mjs
+++ container.mjs
@@ -1,5 +1,24 @@
 import { html, render } from 'https://dev.jspm.io/lit-html';
 
+const input = ({value, onChange}) => {
+  console.log(onChange)
+  return html`<input 
+    value=${value} 
+    onchange=${(e) => {
+      console.log(e, onChange)
+      onChange(e)
+    }}
+  ></input>`
+}
+
 export const container = () => {
-  return html`this is container`
+  let inputVal = "val"
+  const onChange = (e) => {
+    console.log(e)
+    inputVal = e.target.value
+  }
+  return html`
+    <div>${inputVal}</div>
+    ${input({value: inputVal, onChange}) }
+  `
 }
