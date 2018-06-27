c64ce236b74d34d8200f704dc5615d59373ac9df
terrierscript <13282103+terrierscript@users.noreply.github.com>
2018-06-27 15:45:09 +0900

    add

--- bootstrap.js
+++ bootstrap.js
@@ -3,7 +3,7 @@ const fs = require("fs")
 const path = require("path")
 // import promisify from "promisify"
 const Fiber = require("fibers");
-
+const axios = require("axios")
 const bsRoot = "./node_modules/bootstrap/scss"
 // const baseBs = fs.readFileSync(
 //   path.resolve(bsRoot, "bootstrap.scss"),
@@ -18,28 +18,45 @@ const buildParams = (params) => {
 // 
 const scssString = (append) => {
   // const url = "node_modules/bootstrap/scss/bootstrap"
-  const url = "https://unpkg.com/bootstrap@4.1.1/scss/bootstrap.scss"
+  const url = "bootstrap"
+  // const url = "url(https://unpkg.com/bootstrap@4.1.1/scss/bootstrap.scss)"
   const scss = `
   ${append}
   @import "${url}";
-  
   `
   return scss
 }
 
+const getFile = (url) => {
+  return axios.get(url)
+    .then(r => {
+      return r.data
+    })
+}
 exports.build = (variables = {}) => {
   console.log(variables)
   const vars = buildParams(variables);
-  const result = sass.renderSync({
-    data: scssString(vars),
-    importer: function(url, prev, done) {
-      // ...
-      console.log(url)
-    },
-    fiber: Fiber,
-    includePaths: [
-      bsRoot
-    ]
+  const scss = scssString(vars)
+  return new Promise( (res, rej) => {
+    return sass.render({
+      data: scss,
+      importer: (url, prev, done ) => {
+        // ...
+        const name = (url !== "bootstrap") ? `_${url}` : url
+        const unpkgUrl = `https://unpkg.com/bootstrap@4.1.1/scss/${name}.scss`
+        getFile(unpkgUrl)
+          .then(r => done({ contents: r}) )
+        // return { contents: "" }
+      },
+      fiber: Fiber,
+      includePaths: [
+        bsRoot
+      ]
+    }, (err, result) =>{
+      if(err){
+        rej(err)
+      }
+      return res(result.css.toString())
+    })
   })
-  return result.css.toString()
 }
--- test.js
+++ test.js
@@ -1,3 +1,19 @@
 const { build } = require("./bootstrap.js")
 
-console.log(build())
+build().then( css => {
+  console.log(css)
+}).catch(err => {
+  console.error(err)
+})
+
+
+// const sass = require("sass")
+
+// sass.renderSync({
+// 	data: '@import "http://file.sass";',
+// 	importer: url => {
+//     console.log("impr", url);
+
+// 		return { contents: '$foo: bar;' };
+// 	},
+// });
