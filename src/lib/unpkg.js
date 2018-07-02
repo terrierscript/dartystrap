91bb47a054737bc7c57c537afd9156c37805454e
terrierscript <13282103+terrierscript@users.noreply.github.com>
2018-07-02 19:01:50 +0900

    wip

--- src/lib/unpkg.js
+++ src/lib/unpkg.js
@@ -34,6 +34,8 @@ module.exports = packageName => {
       const files = flatten(r.files)
       const resolver = new _UnpkgFetcher(packageName, "4.1.1", files)
       return (url, prev, done) => {
+        console.log(url, prev)
+
         const filename = resolver.resolveFilename(url, prev)
         fetch(filename)
           .then(r => r.text())
