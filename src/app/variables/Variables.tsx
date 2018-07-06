b4e917d51db1e4c76373b3087192e4fb2fdb4d79
terrierscript <13282103+terrierscript@users.noreply.github.com>
2018-07-06 17:55:14 +0900

    variables

--- src/app/variables/Variables.tsx
+++ src/app/variables/Variables.tsx
@@ -44,24 +44,40 @@ class InternalVariablesContainer extends Component<Props, State> {
   }
 }
 
+const getType = (value) => {
+  if (value === "true" || value === "false") {
+    return "checkbox"
+  }
+  return value.indexOf("#") === 0 ? "color" : "input"
+}
 const VariableInput: SFC<{
   variable: VariableType
   onChangeVariable: VariableChangeHandler
-}> = ({ variable, onChangeVariable }) => (
-  <div key={variable.name}>
-    <label>{variable.name}</label>
-    <input
-      placeholder={variable.defaultValue}
-      value={variable.value}
-      onChange={(e) => {
-        onChangeVariable({
-          ...variable,
-          value: e.target.value
-        })
-      }}
-    />
-  </div>
-)
+}> = ({ variable, onChangeVariable }) => {
+  const type = getType(variable.defaultValue)
+  const value = variable.value || variable.defaultValue
+  const inputProps = {
+    type,
+    value,
+    placeholder: variable.defaultValue,
+    checked: value === "true"
+  }
+  return (
+    <div>
+      <label>
+        {variable.name}
+        <input
+          {...inputProps}
+          onChange={(e) => {
+            const newValue =
+              type === "checkbox" ? e.target.checked : e.target.value
+            onChangeVariable({ ...variable, value: newValue.toString() })
+          }}
+        />
+      </label>
+    </div>
+  )
+}
 
 const VariableForm: SFC<VariableContainerChildren> = ({
   variables,
