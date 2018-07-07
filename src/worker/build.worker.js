420cc13a27679c2aa065ccdd7462d9ca00760145
terrierscript <13282103+terrierscript@users.noreply.github.com>
2018-07-07 17:19:48 +0900

    add

--- src/worker/build.worker.js
+++ src/worker/build.worker.js
@@ -1,6 +1,44 @@
 import "./polyfill"
 import { build } from "../lib/bootstrap"
 
+console.log(self.modules)
+self.modules = {
+  fs() {
+    return {
+      statSync(name) {
+        return {
+          isFile: () => {
+            return false
+          },
+          isDirectory: () => {
+            return false
+          }
+        }
+      }
+    }
+  }
+}
+self.require = (name) => {
+  console.log("poly require", name)
+  if (name !== "fs") {
+    return false
+  }
+  const fsMock = {
+    statSync(name) {
+      return {
+        isFile: () => {
+          return false
+        },
+        isDirectory: () => {
+          return false
+        }
+      }
+    }
+  }
+  return fsMock
+}
+console.log("require", require)
+
 // console.log("polyfill", setImmediate)
 // console.log("Buffer", Buffer)
 
