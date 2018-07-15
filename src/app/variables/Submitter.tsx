7e51a2a8b0206af30bafdff5c42395f217e88457
terrierscript <13282103+terrierscript@users.noreply.github.com>
2018-07-15 18:41:36 +0900

    nope

--- src/app/variables/Submitter.tsx
+++ src/app/variables/Submitter.tsx
@@ -9,28 +9,28 @@ type ChildProps<T> = {
 type Props<T> = { item: T; children: (props: ChildProps<T>) => ReactNode }
 type State<T> = {
   item: T | {}
-  realtime: boolean
+  noopMode: boolean
 }
 
 export class Submitter<T> extends Component<Props<T>, State<T>> {
   constructor(props) {
     super(props)
     // TODO
-    this.state = { item: {}, realtime: false } // = { item: this.props.item }
+    this.state = { item: {}, noopMode: false } // = { item: this.props.item }
   }
   handleClick = () => {
     this.setState({ item: this.props.item })
   }
   // update only state update
   shouldComponentUpdate(_, nextState) {
-    if (this.state.realtime) {
+    if (this.state.noopMode) {
       return true
     }
     const stateUpdated = this.state !== nextState
     return stateUpdated
   }
   render() {
-    const item = this.state.realtime ? this.props.item : this.state.item
+    const item = this.state.noopMode ? this.props.item : this.state.item
     return this.props.children({
       item,
       onSubmit: this.handleClick
